a8ad7409913a6c5204b5f7e844e6942a6bf0cb4d
moxito-creator <moxiq2@gmail.com>
2025-12-02 03:02:25 +0300

    Мой первый сайт: HTML, CSS, JavaScript

---
+++ script.js
@@ -0,0 +1,25 @@
+// 1. Находим кнопку на странице
+const button = document.getElementById('myButton');
+
+// 2. Добавляем действие при клике
+button.addEventListener('click', function() {
+    // 3. Меняем текст кнопки
+    button.textContent = 'Ура! Нажато!';
+    
+    // 4. Меняем цвет фона страницы
+    document.body.style.backgroundColor = 'lightgreen';
+    
+    // 5. Выводим сообщение в консоль
+    console.log('Кнопка была нажата!');
+});
+const resetButton = document.getElementById('resetButton');
+
+resetButton.addEventListener('click', function() {
+    // Возвращаем первоначальный текст первой кнопке
+    button.textContent = 'Нажми меня';
+    
+    // Возвращаем первоначальный цвет фона
+    document.body.style.backgroundColor = 'lightblue';
+    
+    console.log('Всё сброшено!');
+});
